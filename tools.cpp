4130d65166d8b468394dd15cd95590b6478f40b8
szym0 <lottigszymon@gmail.com>
2025-12-09 20:20:38 +0100

    09.12.2025

--- tools.cpp
+++ tools.cpp
@@ -10,11 +10,47 @@
 #include <errno.h>
 #include "tools.h"
 
-// semafory
+// SEMAFORY
 
-int create_semaphores(int key, int number) {
+int create_semaphores(int key, int number) { // tworzenie semaforow
 
     int id = semget(key, number, 0666 | IPC_CREAT); // 0666 - prawa zapisu/odczytu dla wszystkich
 
-w
+    if (id == -1) {
+        perror("Error creating semaphores");
+        exit(1);
+    }
+    return id;
+
+}
+
+void remove_semaphores(int sem_id) { // usuwanie semaforow
+    if (semctl(sem_id, 0, IPC_RMID) == -1) { // IPC_RMID = remove id
+        perror("Error removing semaphores");
+    }
+}
+
+void lock_sem(int sem_id, int sem_num) { // zablokowanie semafora (oczekiwanie badz ruszenie)
+    struct sembuf op;
+    op.sem_num = sem_num; // numer semafora
+    op.sem_op = -1;       // -1 oznacza zajecie miejsce, jezeli = 0 zatrzymuje proces
+    op.sem_flg = 0;       // standard
+
+    if (semop(sem_id, &op, 1) == -1) {
+        perror("Error locking semaphore");
+        exit(1);
+    }
+}
+
+void unlock_sem(int sem_id, int sem_num) {
+    // odblokowanie semafora
+    struct sembuf op;
+    op.sem_num = sem_num;
+    op.sem_op = 1;        // +1 oznacza "zwolnij" czyli zwieksza licznik
+    op.sem_flg = 0;
+
+    if (semop(sem_id, &op, 1) == -1) {
+        perror("Error unlocking semaphore");
+        exit(1);
+    }
 }
